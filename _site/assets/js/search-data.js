ae9563d9b65d7107a90861c728d893358073ede7
Andy <andy@emageai.com>
2020-03-27 17:17:01 +0700

    Prototype site

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "Welcome to AI Notes!",
+        "excerpt":"Hello! ","categories": ["other"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2020/03/27/welcome-to-jekyll.html"
+        "url": "http://localhost:4000/other/2020/03/27/welcome-to-ai-notes.html"
       }]
